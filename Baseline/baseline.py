37fa8f71f8dbe315dd3b9e5a95b7ab7d5d03f200
Ferch42 <fernando.hsieh@usp.br>
2018-09-25 09:17:36 -0300

    Algoritmo Baseline

---
+++ Baseline/baseline.py
@@ -0,0 +1,31 @@
+import nltk
+import pickle
+import os
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.cluster import KMeans
+import numpy as np
+from sklearn import metrics
+from sklearn.metrics import silhouette_score
+
+data = []
+
+stopwordz = nltk.corpus.stopwords.words('portuguese')
+
+for d in os.listdir("../../CidadaoData/2017/Dezembro"):
+	dict = pickle.load(open("../../CidadaoData/2017/Dezembro/"+d,"rb"))
+	data.append(dict["reclamacao"].strip())
+
+CV = CountVectorizer(analyzer="word",preprocessor=None,stop_words=stopwordz) 
+
+td = CV.fit_transform(text for text in data)
+
+kmeans = KMeans(n_clusters=2, verbose=1).fit(td)
+
+TV = TfidfVectorizer(analyzer="word",preprocessor=None,stop_words=stopwordz)
+
+td = TV.fit_transform(text for text in data)
+
+kmeans = KMeans(n_clusters=2, verbose=1).fit(td)
+
+labels = kmeans.labels_
+print(metrics.silhouette_score(td, labels, metric='euclidean'))
