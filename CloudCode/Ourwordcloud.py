73ddfbae339ca601c671f2ce07ab2a14271f7e52
AugustoCalado <augusto.bueno@usp.br>
2018-10-02 11:59:44 -0300

    [WordCloud] Resolvendo Problemas de Save
    Arquivo será salvo na pasta RP/Baseline
    
    Signed-off-by: AugustoCalado <augusto.bueno@usp.br>

--- CloudCode/Ourwordcloud.py
+++ CloudCode/Ourwordcloud.py
@@ -5,18 +5,9 @@ import GroupedColorFunc
 from wordcloud import WordCloud, get_single_color_func
 import PalavrasMaisFrequentesPorCluster
 
-class SimpleGroupedColorFunc(object):
+class Ourwordcloud():
 
-    def __init__(self, color_to_words, default_color):
-        self.word_to_color = {word: color
-                              for (color, words) in color_to_words.items()
-                              for word in words}
-
-        self.default_color = default_color
-
-    def __call__(self, word, **kwargs):
-        return self.word_to_color.get(word, self.default_color)
-
+    
     def get_lista_cores(self):
         cores = ['midnightblue','salmon','red','green','purple','yellow','cyan']
         return cores
@@ -29,8 +20,11 @@ class SimpleGroupedColorFunc(object):
     def multiplicacao_das_palavras(self, lista_palavras_mais_frequentes_clusterizadas):
         text = ""
         for  palavra, frequencia in lista_palavras_mais_frequentes_clusterizadas:
-            for i in range(frequencia):
-                text = text + " " + palavra
+            print(palavra,frequencia)
+            if frequencia > 2 :
+                for i in range(frequencia):
+                    text = text + " " + palavra
+                    print(text)
         return text  
 
     def gerar_wordcloud_e_salvar(self, lista_palavras_mais_frequentes_clusterizadas, nome_do_arquivo):# lista_palavras_mais_frequentes_clusterizados [[(palavra1_cluster1,freq)],[(palavra1_cluster2,freq)]]
@@ -39,14 +33,23 @@ class SimpleGroupedColorFunc(object):
         for cluster, palavras_do_cluster in enumerate(lista_palavras_mais_frequentes_clusterizadas):
             cor = random.choice(colors)
             colors.remove(cor)
+            #print("Antes Concantenar Palavras")
             palavras_concatenadas = self.multiplicacao_das_palavras(palavras_do_cluster)
+            #print(palavras_concatenadas)
+            #teste = [palav[0] for palav in palavras_do_cluster]
+            #print(teste)
+            #color_to_words = self.colorir_palavras_de_um_cluster(teste, cor)
             color_to_words = self.colorir_palavras_de_um_cluster(palavras_concatenadas, cor)
-            grouped_color_func = GroupedColorFunc.GroupedColorFunc(palavras_concatenadas, default_color)
-
+            #print(color_to_words)
+            grouped_color_func = GroupedColorFunc.GroupedColorFunc(color_to_words, default_color)
+            #print(grouped_color_func)
             wc = WordCloud(width = 2000,height = 1800, background_color = 'white', min_font_size = 5, relative_scaling = 1 ).generate(palavras_concatenadas)
+            #print("Depois WordCloud")
+            
             wc.recolor(color_func = grouped_color_func)
             plt.figure()
             plt.imshow(wc, interpolation="bilinear")
             plt.axis("off")
+            plt.show()
             wc.to_file(nome_do_arquivo + "_cluster_" + str(cluster+1) + ".png")
         
