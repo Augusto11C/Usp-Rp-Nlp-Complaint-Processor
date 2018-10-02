c138c1692be4aebf5bed6979ddd2290e3beb1742
lucaslai10 <lucaslai10@yahoo.com.br>
2018-10-02 09:00:03 -0300

    problemas com import

--- Baseline/baseline.py
+++ Baseline/baseline.py
@@ -6,6 +6,11 @@ from sklearn.cluster import KMeans
 import numpy as np
 from sklearn import metrics
 from sklearn.metrics import silhouette_score
+import sys
+sys.path.insert(0, '../CloudCode')
+import wordcloud
+import PalavrasMaisFrequentesPorCluster
+import GroupedColorFunc
 
 data = []
 
@@ -29,3 +34,6 @@ kmeans = KMeans(n_clusters=2, verbose=1).fit(td)
 
 labels = kmeans.labels_
 print(metrics.silhouette_score(td, labels, metric='euclidean'))
+
+
+
