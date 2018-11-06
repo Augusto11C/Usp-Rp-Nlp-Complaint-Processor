c2a6d98cdb57c2fe57b34e385af58bbc4b5e026c
AugustoCalado <augusto.bueno@usp.br>
2018-11-06 12:00:37 -0200

    LDA implementation

--- LDA/SMELLY_LDA.py
+++ LDA/SMELLY_LDA.py
@@ -21,102 +21,127 @@ proportional to (n_samples * iterations).
 
 """
 
-# Author: Olivier Grisel <olivier.grisel@ensta.org>
-#         Lars Buitinck
-#         Chyi-Kwei Yau <chyikwei.yau@gmail.com>
-# License: BSD 3 clause
-
 from __future__ import print_function
 from time import time
 
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
-from sklearn.datasets import fetch_20newsgroups
+import pickle, os
+import re
+
+import nltk
 
 n_samples = 2000
-n_features = 1000
+n_features = 3000
 n_components = 10
 n_top_words = 20
 
+BASEFOLDER = '../../CategorizedWaifus/'
+
+
+def load_waifus(dump=False):
+    waifu_list = []
+    waifu_path_list = []
+    for year in os.listdir(BASEFOLDER):
+        # if "waifu" in year:
+        #     continue
+        for month in os.listdir(BASEFOLDER + "/" + year):
+            for waifu_name in os.listdir(BASEFOLDER + "/" + year + "/" + month):
+                with open(BASEFOLDER + "/" + year + "/" + month + "/" + waifu_name, 'rb') as waifu_file:
+                    waifu = pickle.load(waifu_file)
+                    waifu_list.append(waifu['reclamacao'].strip())
+                    waifu_path_list.append( year + "/" + month + "/" + waifu_name)
+
+    waifu_list = tuple(waifu_list)
+    waifu_path_list = tuple(waifu_path_list)
+
+    if dump:
+        with open("waifu_list.tuple", "wb") as f:
+            pickle.dump(waifu_list,f)
+        with open("waifu_path_list.tuple", "wb") as f:
+            pickle.dump(waifu_path_list,f)
+    
+    return waifu_list, waifu_path_list
+
+
+if __name__ == "__main__":
+    waifu_alr_exists = False
+    for file_name in os.listdir('.'):
+        if re.search("waifu", file_name):
+            waifu_alr_exists = True
+            print("Waifus already saved; using existing ones")
+    if waifu_alr_exists:
+        with open("waifu_list.tuple", "rb") as f:
+            waifu_list = pickle.load(f)
+        with open("waifu_path_list.tuple", "rb") as f:
+            waifu_path_list = pickle.load(f)
+    else:
+        waifu_list, waifu_path_list = load_waifus(dump=True)
+
+
+    lda_from_waifus(waifu_list, 6)
+
+    
+
+    pass
 
 def print_top_words(model, feature_names, n_top_words):
     for topic_idx, topic in enumerate(model.components_):
-        message = "Topic #%d: " % topic_idx
-        message += " ".join([feature_names[i]
-                             for i in topic.argsort()[:-n_top_words - 1:-1]])
-        print(message)
+        print("Topic #%d:" % topic_idx)
+        print(" ".join([feature_names[i]
+                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
     print()
 
+def lda_from_waifus(waifu_list: tuple, n_topics=8):      
+    print("Loading dataset...")
+    t0 = time()
+    # Using tf-idf features for LDA
+    stopwords = nltk.corpus.stopwords.words('portuguese')+["","\r\n","pois","que","pra","ter","fazer","ser","para"]
+    print("Extracting tf-idf features from the waifus...")
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.95,
+                                       max_features=n_features,
+                                       stop_words=stopwords)
 
-# Load the 20 newsgroups dataset and vectorize it. We use a few heuristics
-# to filter out useless terms early on: the posts are stripped of headers,
-# footers and quoted replies, and common English words, words occurring in
-# only one document or in at least 95% of the documents are removed.
+    tfidf = tfidf_vectorizer.fit_transform(waifu for waifu in waifu_list)
+    print("done in %0.3fs." % (time() - t0))
+    
+    # Use tf (raw term count) features for LDA.
+    # print("Extracting tf features for LDA...")
+    # tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2,
+    #                                 max_features=n_features,
+    #                                 stop_words='english')
+    # t0 = time()
+    # tf = tf_vectorizer.fit_transform(data_samples)
+    # print("done in %0.3fs." % (time() - t0))
 
-print("Loading dataset...")
-t0 = time()
-dataset = fetch_20newsgroups(shuffle=True, random_state=1,
-                             remove=('headers', 'footers', 'quotes'))
-data_samples = dataset.data[:n_samples]
-print("done in %0.3fs." % (time() - t0))
+    # Fit the NMF model
+    # print("Fitting the NMF model (Frobenius norm) with tf-idf features, "
+    #       "n_samples=%d and n_features=%d..."
+    #       % (n_samples, n_features))
+    # t0 = time()
+    # nmf = NMF(n_components=n_components, random_state=1,
+    #           alpha=.1, l1_ratio=.5).fit(tfidf)
+    # print("done in %0.3fs." % (time() - t0))
+    # Fit the NMF model
+    # print("Fitting the NMF model (generalized Kullback-Leibler divergence) with "
+    #       "tf-idf features, n_samples=%d and n_features=%d..."
+    #       % (n_samples, n_features))
+    #     # t0 = time()
+    # nmf = NMF(n_components=n_components, random_state=1,
+    #           beta_loss='kullback-leibler', solver='mu', max_iter=1000, alpha=.1,
+    #           l1_ratio=.5).fit(tfidf)
+    # print("done in %0.3fs." % (time() - t0))
+    tfidf_featurte_names = tfidf_vectorizer.get_feature_names()#matriz, linha palavra coluna documento
+    print("Fitting LDA models with tf features, "
+          "n_samples=%d and n_features=%d..."
+          % (n_samples, n_features))
+    lda = LatentDirichletAllocation(n_components=n_topics, max_iter=5,
+                                    learning_method='online',
+                                    learning_offset=50.,
+                                    random_state=0)
+    t0 = time()
+    lda.fit(tfidf)
+    print("done in %0.3fs." % (time() - t0))
+    print("\nTopics in LDA model:")
+    print_top_words(lda, tfidf_featurte_names, 5)
 
-# Use tf-idf features for NMF.
-print("Extracting tf-idf features for NMF...")
-tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2,
-                                   max_features=n_features,
-                                   stop_words='english')
-t0 = time()
-tfidf = tfidf_vectorizer.fit_transform(data_samples)
-print("done in %0.3fs." % (time() - t0))
-
-# Use tf (raw term count) features for LDA.
-print("Extracting tf features for LDA...")
-tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2,
-                                max_features=n_features,
-                                stop_words='english')
-t0 = time()
-tf = tf_vectorizer.fit_transform(data_samples)
-print("done in %0.3fs." % (time() - t0))
-print()
-
-# Fit the NMF model
-print("Fitting the NMF model (Frobenius norm) with tf-idf features, "
-      "n_samples=%d and n_features=%d..."
-      % (n_samples, n_features))
-t0 = time()
-nmf = NMF(n_components=n_components, random_state=1,
-          alpha=.1, l1_ratio=.5).fit(tfidf)
-print("done in %0.3fs." % (time() - t0))
-
-print("\nTopics in NMF model (Frobenius norm):")
-tfidf_feature_names = tfidf_vectorizer.get_feature_names()
-print_top_words(nmf, tfidf_feature_names, n_top_words)
-
-# Fit the NMF model
-print("Fitting the NMF model (generalized Kullback-Leibler divergence) with "
-      "tf-idf features, n_samples=%d and n_features=%d..."
-      % (n_samples, n_features))
-t0 = time()
-nmf = NMF(n_components=n_components, random_state=1,
-          beta_loss='kullback-leibler', solver='mu', max_iter=1000, alpha=.1,
-          l1_ratio=.5).fit(tfidf)
-print("done in %0.3fs." % (time() - t0))
-
-print("\nTopics in NMF model (generalized Kullback-Leibler divergence):")
-tfidf_feature_names = tfidf_vectorizer.get_feature_names()
-print_top_words(nmf, tfidf_feature_names, n_top_words)
-
-print("Fitting LDA models with tf features, "
-      "n_samples=%d and n_features=%d..."
-      % (n_samples, n_features))
-lda = LatentDirichletAllocation(n_components=n_components, max_iter=5,
-                                learning_method='online',
-                                learning_offset=50.,
-                                random_state=0)
-t0 = time()
-lda.fit(tf)
-print("done in %0.3fs." % (time() - t0))
-
-print("\nTopics in LDA model:")
-tf_feature_names = tf_vectorizer.get_feature_names()
-print_top_words(lda, tf_feature_names, n_top_words)
