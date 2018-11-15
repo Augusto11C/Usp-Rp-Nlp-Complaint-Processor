32e6b22926659784c68d6680fc48126f40d3fc78
AugustoCalado <augusto.bueno@usp.br>
2018-11-15 16:31:54 -0200

    added chines + ediçoes palavras

---
+++ Baseline/run_chinese.py
@@ -0,0 +1,56 @@
+import nltk
+import pickle
+import os
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.cluster import KMeans
+import numpy as np
+from sklearn import metrics
+from sklearn.metrics import silhouette_score
+import sys
+
+sys.path.insert(0, '..\CloudCode')
+from Ourwordcloud import Ourwordcloud
+# import Ourwordcloud
+from PalavrasMaisFrequentesPorCluster import PalavrasMaisFrequentesPorCluster
+
+import GroupedColorFunc
+
+sys.path.insert(0,"..\PreProcessor")
+from Preprocessor import Preprocessor
+
+
+sys.path.insert(0, '..\RestauranteChinesDePalavras')
+from yakisoba_do_chifu import yakisoba_do_chifu
+
+#mega texto tokeniza
+
+#dict_keys(['data', 'kmeans', 'scores'])
+
+
+
+kmeans_normal = pickle.load(open("../../KMEANS_BASE/resultados_kmeans.jojo","rb"))
+
+#kmeans_w2c = pickle.load(open("../../KMEANS_BASE/resultados_kmeans_word2vec.jojo"))
+kmeans_do_arqKmeans = kmeans_normal['kmeans']
+data_do_arqKmeans = kmeans_normal['data']
+
+obj_clusterizado = "augusto"
+obj_clusterizado = PalavrasMaisFrequentesPorCluster(data_do_arqKmeans).get_corpus_clusterizado(data_do_arqKmeans, kmeans_do_arqKmeans[1])
+
+if (obj_clusterizado == "augusto"):
+    print("AUguto")
+
+palavras_clusterizadas = PalavrasMaisFrequentesPorCluster(data_do_arqKmeans).get_palavras_clusterizadas(obj_clusterizado)
+print(palavras_clusterizadas[1])
+
+for kmean_aq in kmeans_do_arqKmeans:
+    obj_clusterizado = PalavrasMaisFrequentesPorCluster(data_do_arqKmeans).get_corpus_clusterizado(data_do_arqKmeans,kmean_aq)
+    palavras_clusterizadas = PalavrasMaisFrequentesPorCluster(data_do_arqKmeans).get_palavras_clusterizadas(obj_clusterizado)
+    for lista_palavras in palavras_clusterizadas:
+        yakisoba_do_chifu = yakisoba_do_chifu()
+        yakisoba_do_chifu.tirar_pedido(self, lista_de_palavras_tokenizadas):
+        yakisoba_do_chifu.prepara_os_ingredientes()
+        yakisoba_do_chifu.cozinhar()
+        yakisoba_do_chifu.empratar()
+        yakisoba_do_chifu.calcular_media_dos_clusters()
+
--- CloudCode/PalavrasMaisFrequentesPorCluster.py
+++ CloudCode/PalavrasMaisFrequentesPorCluster.py
@@ -111,3 +111,48 @@ class PalavrasMaisFrequentesPorCluster:
         corpus_clusterizado = PalavrasMaisFrequentesPorCluster.__get_corpus_clusterizado(corpus,kmeans)
         palavras_clusterizadas = PalavrasMaisFrequentesPorCluster.__get_palavras_clusterizadas(corpus_clusterizado)
         return PalavrasMaisFrequentesPorCluster.__get_n_palavras_mais_frequentes_cluster(n, palavras_clusterizadas)
+
+    def get_corpus_clusterizado(self, corpus, kmeans):
+        corpus_clusterizado = [[] for _ in range(kmeans.n_clusters)]
+        for index, n_cluster in zip([i for i in range(len(kmeans.labels_))] ,kmeans.labels_):
+            corpus_clusterizado[n_cluster].append(corpus[index])
+        return corpus_clusterizado
+
+        """recebe a lista de listas de textos, transforma a lista de textos em lista de palavras 
+    e devolve lista de listas de palavras, em que cada lista interna representa um cluster"""
+    def get_palavras_clusterizadas(self, corpus_clusterizado):
+        '''palavras_clusterizadas = [[] for _ in range(len(corpus_clusterizado))]
+        for n_cluster,cluster in enumerate(corpus_clusterizado):
+            string = ""
+            for texto in cluster:
+                string = string + " " + texto
+                palavras = string.split(" ")
+                for palavra in palavras:
+                    palavras_clusterizadas[n_cluster].append(palavra)
+        #return palavras_clusterizadas
+        stopwordz = nltk.corpus.stopwords.words('portuguese')+["","\r\n","pois","que","pra","ter","fazer","ser","para"]        
+        
+        palavras_clusterizadas = [[] for _ in range(len(corpus_clusterizado))]
+        for n_cluster,cluster in enumerate(corpus_clusterizado):
+            string = ""
+            for texto in cluster:
+                string = string + " " + texto
+                palavras = re.sub('[^ a-zA-Z0-9]', ' ', string)
+        
+            palavras = palavras.split(" ")
+            
+            for palavra in palavras:
+                word = palavra.replace("\\s+","")
+                
+                if(word not in stopwordz and not len(word) <= 2 and not word.isdigit()):
+                    palavras_clusterizadas[n_cluster].append(word)'''
+        preprocessor = Preprocessor
+        palavras_clusterizadas = [[] for _ in range(len(corpus_clusterizado))]
+        for n_cluster,cluster in enumerate(corpus_clusterizado):
+            palavras_tokenizadas = []
+            for texto in cluster:
+                texto_preprocessado = preprocessor.preprocess(texto)
+                palavras_tokenizadas = palavras_tokenizadas + preprocessor.tokenize_string(texto_preprocessado)
+            palavras_clusterizadas[n_cluster] = palavras_clusterizadas[n_cluster] + palavras_tokenizadas
+
+        return palavras_clusterizadas
