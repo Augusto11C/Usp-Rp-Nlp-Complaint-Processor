29263101671729a03f7176a9a0e31720c9878537
lucaslai10 <lucaslai10>
2018-11-18 19:54:03 -0200

    plotar kmeans atualizado

--- PosProcessamento/PlotarKmeans.py
+++ PosProcessamento/PlotarKmeans.py
@@ -19,15 +19,19 @@ import GroupedColorFunc
 sys.path.insert(2, '..\TokenizadorDeCorpus')
 
 
-resultados_kmeans = pickle.load(open('resultados_kmeans.jojo','rb'))
-corpus_tokenizado = pickle.load(open('..\TokenizadorDeCorpus\corpus_tokenizado.lai','rb'))
+resultados_kmeans = pickle.load(open('..\TokenizadorDeCorpus\\resultados_kmeans_word2vec_sem_java.jojo','rb'))
+corpus_tokenizado = pickle.load(open('..\TokenizadorDeCorpus\corpus_tokenizado_sem_java.lai','rb'))
 print(resultados_kmeans['kmeans'])
 data = resultados_kmeans['data']
 lista_kmeans = resultados_kmeans['kmeans']
 palavrasMaisFrequentesPorCluster2 = PalavrasMaisFrequentesPorCluster2(corpus_tokenizado)
+i = 5
 for kmeans in lista_kmeans:
+	'''if(i <= 9):
+		i = i+1
+		continue'''
 	print(kmeans)
 	lista_das_palavras_mais_frequentes_por_cLuster = palavrasMaisFrequentesPorCluster2.gerar_n_palavras_mais_frequentes_por_cluster(20,kmeans)
-	Ourwordcloud().gerar_wordcloud_e_salvar(lista_palavras_mais_frequentes_clusterizadas = lista_das_palavras_mais_frequentes_por_cLuster, nome_do_arquivo = "kmeans_tfidf_k=" + str(kmeans.n_clusters))
-
+	Ourwordcloud().gerar_wordcloud_e_salvar(lista_palavras_mais_frequentes_clusterizadas = lista_das_palavras_mais_frequentes_por_cLuster, nome_do_arquivo = "kmeans_word2vec_sem_java_k=" + str(kmeans.n_clusters))
+	#i = i+1
 
